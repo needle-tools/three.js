40598002a583c1b182a59abf949439784ee3bd0b
Mr.doob <info@mrdoob.com>
2023-06-29 12:02:33 +0900

    Needle changes up to r154
    (cherry picked from commit d0a897af878d96f2b19e38c4e552f70a33afc47b)

--- examples/jsm/exporters/GLTFExporter.js
+++ examples/jsm/exporters/GLTFExporter.js
@@ -1681,9 +1681,11 @@ class GLTFWriter {
 		}
 
 		// pbrMetallicRoughness.metallicRoughnessTexture
-		if ( material.metalnessMap || material.roughnessMap ) {
+		const metalnessMap = material.metalnessMap?.image ? material.metalnessMap : undefined;
+		const roughnessMap = material.roughnessMap?.image ? material.roughnessMap : undefined;
+		if ( metalnessMap || roughnessMap ) {
 
-			const metalRoughTexture = await this.buildMetalRoughTextureAsync( material.metalnessMap, material.roughnessMap );
+			const metalRoughTexture = await this.buildMetalRoughTextureAsync( metalnessMap, roughnessMap );
 
 			const metalRoughMapDef = {
 				index: await this.processTextureAsync( metalRoughTexture ),
@@ -1718,7 +1720,7 @@ class GLTFWriter {
 			}
 
 			// emissiveTexture
-			if ( material.emissiveMap ) {
+			if ( material.emissiveMap?.image ) {
 
 				const emissiveMapDef = {
 					index: await this.processTextureAsync( material.emissiveMap ),
@@ -1732,7 +1734,7 @@ class GLTFWriter {
 		}
 
 		// normalTexture
-		if ( material.normalMap ) {
+		if ( material.normalMap?.image ) {
 
 			const normalScale = material.normalScale;
 
@@ -1773,7 +1775,7 @@ class GLTFWriter {
 		}
 
 		// occlusionTexture
-		if ( material.aoMap ) {
+		if ( material.aoMap?.image ) {
 
 			const occlusionMapDef = {
 				index: await this.processTextureAsync( material.aoMap ),
