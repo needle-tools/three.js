88db7daa8738ab9ef99edb84c1730dd89aae6fde
hybridherbst <felix.herbst@gmail.com>
2024-01-18 23:05:24 +0100

    test with custom occlusion shader for depth intersections

--- src/renderers/shaders/ShaderChunk/occlusion_fragment.glsl.js
+++ src/renderers/shaders/ShaderChunk/occlusion_fragment.glsl.js
@@ -14,7 +14,21 @@ export default /* glsl */`
         //float occlusion = Depth_GetOcclusion(depthColor, depthUv, assetDepthM, arrayIndex);
         float occlusion = Depth_GetBlurredOcclusionAroundUV(depthColor, depthUv, assetDepthM, arrayIndex);
 
-        gl_FragColor *= 1.0 - occlusion;
+        // distance
+        float depthMm = Depth_GetCameraDepthInMeters(depthColor, depthUv, arrayIndex);
+        
+        float noise = snoise(gl_FragCoord.xy * 0.005);
+        
+        depthMm += noise * 0.002;
+        float absDistance = abs(assetDepthM - depthMm);
+        float v = 0.0025;
+        absDistance = saturate(v - absDistance) / v;
+
+        // perturb absDistance by 3D noise
+        // absDistance += noise * 0.1;
+
+        gl_FragColor.rgb += vec3(absDistance * 2.0, absDistance * 2.0, absDistance * 12.0);
+        gl_FragColor = mix(gl_FragColor, vec4(0.0, 0.0, 0.0, 0.0), occlusion * 0.7);
     }
 #endif
 `;
