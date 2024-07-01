05684a2209168fde97bde02f81099e4a3b8dd0bb
hybridherbst <felix.herbst@gmail.com>
2024-07-01 16:23:09 +0200

    FBXLoader: Fix exception when some rotation curves are undefined, which is currently not supported
    Reproduces with the FBX from https://sketchfab.com/3d-models/painter-of-stars-a690c41b3b8f4c509fbf2ace7b8fb132

--- examples/jsm/loaders/FBXLoader.js
+++ examples/jsm/loaders/FBXLoader.js
@@ -2804,6 +2804,16 @@ class AnimationParser {
 		let times;
 		let values;
 
+		if ( curves.x === undefined || curves.y === undefined || curves.z === undefined ) {
+
+			console.warn( 'THREE.FBXLoader: Missing rotation curves ' +
+				( curves.x === undefined ? 'x ' : '' ) + ( curves.y === undefined ? 'y ' : '' ) + ( curves.z === undefined ? 'z ' : '' ) +
+				' for animation of ' + modelName + '. This is currently not supported.' );
+
+			return undefined;
+
+		}
+
 		if ( curves.x !== undefined && curves.y !== undefined && curves.z !== undefined ) {
 
 			const result = this.interpolateRotations( curves.x, curves.y, curves.z, eulerOrder );
@@ -2841,7 +2851,7 @@ class AnimationParser {
 
 		const quaternionValues = [];
 
-		if ( ! values || ! times ) return new QuaternionKeyframeTrack( modelName + '.quaternion', [ 0 ], [ 0 ] );
+		if ( ! values || ! times ) return undefined;
 
 		for ( let i = 0; i < values.length; i += 3 ) {
 
