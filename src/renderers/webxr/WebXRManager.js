c4566131e74ea04737a442a0745e3bdbe82f6b14
hybridherbst <felix.herbst@gmail.com>
2024-07-03 20:36:20 +0200

    workaround for end-of-session exceptions in quest browser (at least in r66)
    (cherry picked from commit ed09efbe24aef464ac550d08a343c7c58ae53841)

--- src/renderers/webxr/WebXRManager.js
+++ src/renderers/webxr/WebXRManager.js
@@ -229,7 +229,7 @@ class WebXRManager extends EventDispatcher {
 
 				controllerInputSources[ i ] = null;
 
-				controllers[ i ].disconnect( inputSource );
+				if ( controllers[ i ] ) controllers[ i ].disconnect( inputSource );
 
 			}
 
