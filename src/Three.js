bb7b08877fecccbf726cd284df9daac5fbf19e57
hybridherbst <felix.herbst@gmail.com>
2024-07-26 13:16:59 +0200

    clean up WebGLNodes and WebGLNodeBuilder, add Nodes to Three.js again
    (cherry picked from commit 877f111812203754a206a2b6e75cb5b77bfa8650)

--- src/Three.js
+++ src/Three.js
@@ -8,3 +8,45 @@ export { UniformsUtils } from './renderers/shaders/UniformsUtils.js';
 export { ShaderChunk } from './renderers/shaders/ShaderChunk.js';
 export { PMREMGenerator } from './extras/PMREMGenerator.js';
 export { WebGLUtils } from './renderers/webgl/WebGLUtils.js';
+export * from './nodes/Nodes.js';
+<<<<<<< HEAD
+=======
+export { createCanvasElement } from './utils.js';
+export * from './constants.js';
+export * from './Three.Legacy.js';
+export * from './nodes/Nodes.js';
+
+if ( typeof __THREE_DEVTOOLS__ !== 'undefined' ) {
+
+	__THREE_DEVTOOLS__.dispatchEvent( new CustomEvent( 'register', { detail: {
+		revision: REVISION,
+	} } ) );
+
+}
+
+if ( typeof window !== 'undefined' ) {
+
+	try {
+
+		if ( import.meta ) {
+
+			if ( ! window.__THREE__IMPORTS__) window.__THREE__IMPORTS__ = [];
+			window.__THREE__IMPORTS__.push( { url: import.meta.url, revision: REVISION } );
+
+		}
+
+	} catch { }
+
+	if ( window.__THREE__ ) {
+
+		console.warn( 'WARNING: Multiple instances of Three.js being imported. Existing: ' + window.__THREE__ + ', new: ' + REVISION );
+		console.warn( window.__THREE__IMPORTS__ );
+
+	} else {
+
+		window.__THREE__ = REVISION;
+
+	}
+
+}
+>>>>>>> 877f111812 (clean up WebGLNodes and WebGLNodeBuilder, add Nodes to Three.js again)
