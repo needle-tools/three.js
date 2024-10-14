a468a098004fc2585bf2b1e783578326f730015e
hybridherbst <felix.herbst@gmail.com>
2024-10-14 14:55:27 +0200

    export more Nodes things from three

--- src/Three.js
+++ src/Three.js
@@ -7,3 +7,11 @@ export { UniformsUtils } from './renderers/shaders/UniformsUtils.js';
 export { ShaderChunk } from './renderers/shaders/ShaderChunk.js';
 export { PMREMGenerator } from './extras/PMREMGenerator.js';
 export { WebGLUtils } from './renderers/webgl/WebGLUtils.js';
+
+export * from './materials/nodes/NodeMaterials.js';
+export { default as NodeLoader } from './loaders/nodes/NodeLoader.js';
+export { default as NodeObjectLoader } from './loaders/nodes/NodeObjectLoader.js';
+export { default as NodeMaterialLoader } from './loaders/nodes/NodeMaterialLoader.js';
+export * from './nodes/Nodes.js';
+export * from './nodes/TSL.js';
+export { default as BasicNodeLibrary } from './renderers/webgpu/nodes/BasicNodeLibrary.js';
