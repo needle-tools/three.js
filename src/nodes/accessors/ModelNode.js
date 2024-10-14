18cde5576d767d1243b2ae68e0dfe127ca062cab
hybridherbst <felix.herbst@gmail.com>
2024-10-14 14:55:52 +0200

    rename modelViewMatrix node var name to avoid collision with WebGL uniforms

--- src/nodes/accessors/ModelNode.js
+++ src/nodes/accessors/ModelNode.js
@@ -125,7 +125,7 @@ export const modelViewMatrix = /*@__PURE__*/ ( Fn( ( builder ) => {
 
 	return builder.renderer.overrideNodes.modelViewMatrix || mediumpModelViewMatrix;
 
-} ).once() )().toVar( 'modelViewMatrix' );
+} ).once() )().toVar( 'modelViewMatrix_2' );
 
 // GPU Precision
 
