d76ae61d62039e9cbff35b6c47eea91f66dab1ef
Marcel Wiessler <marcel@gaisterhand.de>
2025-01-10 16:53:27 +0100

    Make OrbitControls smooth again

--- examples/jsm/controls/OrbitControls.js
+++ examples/jsm/controls/OrbitControls.js
@@ -388,9 +388,8 @@ class OrbitControls extends Controls {
 
 			}
 
-			this._spherical.radius = this._clampDistance( this._spherical.radius * this._currentScale );
 			const prevRadius = this._spherical.radius;
-			this._spherical.radius = this._clampDistance( this._spherical.radius * this._scale );
+			this._spherical.radius = this._clampDistance( this._spherical.radius * this._currentScale );
 			zoomChanged = prevRadius != this._spherical.radius;
 
 		}
@@ -513,7 +512,6 @@ class OrbitControls extends Controls {
 		}
 
 		this._scale = 1;
-		this._currentScale = 1;
 		this._performCursorZoom = false;
 
 		// update condition is:
