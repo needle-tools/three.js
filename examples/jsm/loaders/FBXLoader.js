9d570ca688b498d554619d27c5ed0874228c1227
hybridherbst <felix.herbst@gmail.com>
2025-02-21 15:34:52 +0100

    Calculate skinned mesh boundingBox/boundingSphere upon FBX loading
    (cherry picked from commit bc03f211b4500905e92178d8e7f9bc59a5f2e75d)

--- examples/jsm/loaders/FBXLoader.js
+++ examples/jsm/loaders/FBXLoader.js
@@ -941,6 +941,16 @@ class FBXTreeParser {
 
 			}
 
+			if (node.isSkinnedMesh) {
+
+				// Compute bounding sphere
+				node.computeBoundingBox();
+				node.computeBoundingSphere();
+				node.boundingBox.expandByScalar( 3 );
+				node.boundingSphere.radius *= 3;
+
+			}
+
 		} );
 
 		const animations = new AnimationParser().parse();
