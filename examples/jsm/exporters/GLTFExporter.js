038da14bcd61f858d677ff1fda7f1cd1839ba770
hybridherbst <felix.herbst@gmail.com>
2025-08-04 15:07:44 +0200

    fix: add support for color_0 morph targets
    (cherry picked from commit b4e8985f67a796437de2bb0e10e695ecf10399af)

--- examples/jsm/exporters/GLTFExporter.js
+++ examples/jsm/exporters/GLTFExporter.js
@@ -2028,14 +2028,14 @@ class GLTFWriter {
 
 				for ( const attributeName in geometry.morphAttributes ) {
 
-					// glTF 2.0 morph supports only POSITION/NORMAL/TANGENT.
+					// glTF 2.0 morph supports only POSITION/NORMAL/TANGENT/COLOR.
 					// Three.js doesn't support TANGENT yet.
 
-					if ( attributeName !== 'position' && attributeName !== 'normal' ) {
+					if ( attributeName !== 'position' && attributeName !== 'normal' && attributeName !== 'color' ) {
 
 						if ( ! warned ) {
 
-							console.warn( 'GLTFExporter: Only POSITION and NORMAL morph are supported.' );
+							console.warn( 'GLTFExporter: Only POSITION, NORMAL and COLOR morph are supported.' );
 							warned = true;
 
 						}
@@ -2045,7 +2045,7 @@ class GLTFWriter {
 					}
 
 					const attribute = geometry.morphAttributes[ attributeName ][ i ];
-					const gltfAttributeName = attributeName.toUpperCase();
+					const gltfAttributeName = attributeName.toUpperCase() + ( attributeName === 'color' ? '_0' : '' );
 
 					// Three.js morph attribute has absolute values while the one of glTF has relative values.
 					//
