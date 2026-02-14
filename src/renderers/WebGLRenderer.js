9717aada5aa76d697c5d98feebecfb9fba59eeb5
Marcel Wiessler <marcel@gaisterhand.de>
2026-02-14 21:18:33 +0100

    material force refresh uniforms flag

--- src/renderers/WebGLRenderer.js
+++ src/renderers/WebGLRenderer.js
@@ -1970,11 +1970,12 @@ class WebGLRenderer {
 
 			}
 
-			if ( material.id !== _currentMaterialId ) {
+			if ( material.id !== _currentMaterialId || material._forceRefresh ) {
 
 				_currentMaterialId = material.id;
 
 				refreshMaterial = true;
+				material._forceRefresh = false;
 
 			}
 
