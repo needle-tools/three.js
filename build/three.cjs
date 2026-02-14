f17e62baf7b4b8be233dcfa7f530d66ae6fc3f8b
Marcel Wiessler <marcel@gaisterhand.de>
2026-02-14 21:20:43 +0100

    version

--- build/three.cjs
+++ build/three.cjs
@@ -30744,11 +30744,12 @@ class WebGLRenderer {
 
 			}
 
-			if ( material.id !== _currentMaterialId ) {
+			if ( material.id !== _currentMaterialId || material._forceRefresh ) {
 
 				_currentMaterialId = material.id;
 
 				refreshMaterial = true;
+				material._forceRefresh = false;
 
 			}
 
