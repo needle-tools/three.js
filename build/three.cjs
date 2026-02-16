3d1b4e1bb2049878e00244dce2a7b2768e13d0bb
Marcel Wiessler <marcel@gaisterhand.de>
2026-02-16 17:53:09 +0100

    version

--- build/three.cjs
+++ build/three.cjs
@@ -6,7 +6,7 @@
 'use strict';
 
 var _documentCurrentScript = typeof document !== 'undefined' ? document.currentScript : null;
-const REVISION = '169';
+const REVISION = '169.17';
 
 const MOUSE = { LEFT: 0, MIDDLE: 1, RIGHT: 2, ROTATE: 0, DOLLY: 1, PAN: 2 };
 const TOUCH = { ROTATE: 0, PAN: 1, DOLLY_PAN: 2, DOLLY_ROTATE: 3 };
