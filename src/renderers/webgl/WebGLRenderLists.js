2cead9f7bc4d2c1628a37baf72e5471f2f3c221a
Marcel Wiessler <marcel@gaisterhand.de>
2026-02-19 13:50:13 +0100

    onBeforeRenderlist nullcheck fix

--- src/renderers/webgl/WebGLRenderLists.js
+++ src/renderers/webgl/WebGLRenderLists.js
@@ -108,7 +108,7 @@ function WebGLRenderList() {
 
 		const renderItem = getNextRenderItem( object, geometry, material, groupOrder, z, group );
 
-		object.onBeforeRenderListPush( object, geometry, material, group );
+		object.onBeforeRenderListPush?.( object, geometry, material, group );
 
 		if ( material.transmission > 0.0 ) {
 
@@ -124,7 +124,7 @@ function WebGLRenderList() {
 
 		}
 
-		object.onAfterRenderListPush( object, geometry, material, group );
+		object.onAfterRenderListPush?.( object, geometry, material, group );
 
 	}
 
