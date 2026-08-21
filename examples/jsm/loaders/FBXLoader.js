155e7c1be2d674cad61e56898e459c6ec3921856
hybridherbst <felix.herbst@gmail.com>
2026-08-21 14:54:48 +0200

    FBXLoader: Support embedded textures in workers
    (cherry picked from commit 1c44abfdfb1a5bfdfcf88b7b614f3450b3387c61)

--- examples/jsm/loaders/FBXLoader.js
+++ examples/jsm/loaders/FBXLoader.js
@@ -374,7 +374,7 @@ class FBXTreeParser {
 		} else { // Binary Format
 
 			const array = new Uint8Array( content );
-			return window.URL.createObjectURL( new Blob( [ array ], { type: type } ) );
+			return URL.createObjectURL( new Blob( [ array ], { type: type } ) );
 
 		}
 
